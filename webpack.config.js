5e54be8adea77e14c6843b214174b711f283f3c4
lanbo.gao <lanbo.gao@gmail.com>
2023-09-20 12:14:45 +0800

    Add webpack build to include external module(DOMParser). build: npx webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'mpd-parser.all.min.js',
+    library: 'mpdParser',
+    // libraryExport: 'default',
+    libraryTarget: 'umd',
+    globalObject: 'this'
+  },
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
